9bf1bb41092ee7307b2e6f16e8774e9d2acc0c76
resteve <resteve@zikzakmedia.com>
2016-01-07 23:03:49 +0100

    Add SAO tasks

--- __init__.py
+++ __init__.py
@@ -5,9 +5,12 @@ from invoke import Collection
 # import here your tryton tasks projects
 import trytontasks_modules
 import trytontasks_gal
+import trytontasks_sao
 from .bootstrap import *
 
 ns = Collection()
 ns.add_collection(Collection.from_module(trytontasks_modules, name='modules'))
-ns.add_collection(Collection.from_module(trytontasks_gal, name='gal'))
+ns.add_collection(Collection.from_module(trytontasks_sao, name='sao'))
+ns.add_collection(Collection.from_module(bootstrap, name='zz'))
+
 ns.add_collection(Collection.from_module(bootstrap, name='zz'))
