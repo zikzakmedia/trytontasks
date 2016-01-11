22139acd38b98c8a02cd8c054c3da3bc0aea377c
resteve <resteve@zikzakmedia.com>
2016-01-11 23:06:44 +0100

    Add User DOC tasks

--- __init__.py
+++ __init__.py
@@ -8,7 +8,16 @@ import trytontasks_gal
 import trytontasks_sao
 from .bootstrap import *
 
+try:
+    import trytontasks_userdoc
+    userdoc = True
+except:
+    userdoc = False
+
 ns = Collection()
 ns.add_collection(Collection.from_module(trytontasks_modules, name='modules'))
 ns.add_collection(Collection.from_module(trytontasks_sao, name='sao'))
 ns.add_collection(Collection.from_module(bootstrap, name='zz'))
+
+if userdoc:
+    ns.add_collection(Collection.from_module(trytontasks_userdoc, name='doc'))
